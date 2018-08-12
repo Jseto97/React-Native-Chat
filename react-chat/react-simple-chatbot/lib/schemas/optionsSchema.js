d6f645efaef9a59b780a8cda65d5ebf09fd462fd
Justin Seto <jsetomsc@gmail.com>
2018-08-11 22:51:39 -0400

    Add files via upload

---
+++ react-chat/react-simple-chatbot/lib/schemas/optionsSchema.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    key: 'id',
+    types: ['string', 'number'],
+    required: true,
+  },
+  {
+    key: 'options',
+    types: ['object'],
+    required: true,
+  },
+  {
+    key: 'end',
+    types: ['boolean'],
+    required: false,
+  },
+  {
+    key: 'metadata',
+    types: ['object'],
+    required: false,
+  },
+];
---
+++ react-chat/react-simple-chatbot/lib/schemas/updateSchema.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    key: 'id',
+    types: ['string', 'number'],
+    required: true,
+  },
+  {
+    key: 'update',
+    types: ['string', 'number'],
+    required: true,
+  },
+  {
+    key: 'trigger',
+    types: ['string', 'number', 'function'],
+    required: true,
+  },
+  {
+    key: 'metadata',
+    types: ['object'],
+    required: false,
+  },
+];
